d43209df351059a00da9da61b1612bf407dcada0
bennetbvarghese <agent98bp@gmail.com>
2024-04-03 18:59:06 +0530

    2 commit

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,7 +2,40 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+      <div className="container form-component login-form">
+        <h2>Sign In</h2>
+        <p>Please Login To Continue</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat culpa
+          voluptas expedita itaque ex, totam ad quod error?
+        </p>
+        <form>
+          <input
+            type="text"
+            placeholder="Email"
+
+          />
+          <input
+            type="password"
+            placeholder="Password"
+ 
+          />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+           
+          />
+          <div
+
+          >
+          </div>
+          <div >
+            <button type="submit">Login</button>
+          </div>
+        </form>
+      </div>
+    </>
   )
 }
 
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -2,7 +2,76 @@ import React from 'react'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <>
+      <div className="container form-component register-form">
+        <h2>Sign Up</h2>
+        <p>Please Sign Up To Continue</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat culpa
+          voluptas expedita itaque ex, totam ad quod error?
+        </p>
+        <form>
+          <div>
+            <input
+              type="text"
+              placeholder="First Name"
+          
+            
+            />
+            <input
+              type="text"
+              placeholder="Last Name"
+           
+              
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              placeholder="Email"
+              
+            />
+            <input
+              type="number"
+              placeholder="Mobile Number"
+              
+            />
+          </div>
+          <div>
+            <input
+              type="number"
+              placeholder="NIC"
+              
+            />
+            <input
+              type="date"
+              placeholder="Date of Birth"
+              
+            />
+          </div>
+          <div>
+            <select >
+              <option value="">Select Gender</option>
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+            </select>
+            <input
+              type="password"
+              placeholder="Password"
+              
+            />
+          </div>
+          <div
+            
+          >
+            <p>Already Registered?</p>
+          </div>
+          <div >
+            <button type="submit">Register</button>
+          </div>
+        </form>
+      </div>
+    </>
   )
 }
 
